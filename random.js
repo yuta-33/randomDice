1644fe24640d8d0f393b9a6461445ea83dfaff33
yuta-33 <yuta33.0501@gmail.com>
2025-01-28 09:27:20 +0900

    Update random.js

--- random.js
+++ random.js
@@ -1,1 +1,8 @@
+import 
+const roll = () =>{
+  const dice = Math.floor(Math.random()*6)+1;
+  document.getElementById('number').textContent = dice;
+}
+document.getElementById('roll').addEventListener('click', roll );
 
+export default random.js from "./random.js"
