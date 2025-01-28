47927439529f3f7ed2061ab8d9971f09476f12d0
yuta-33 <yuta33.0501@gmail.com>
2025-01-28 10:48:21 +0900

    Update random.js

--- random.js
+++ random.js
@@ -1,6 +1,6 @@
 const Dodice = () =>{
   const dice = Math.floor(Math.random()*6)+1;
-  document.getElementById('number').textContent = dice;
+  　document.getElementById('number').textContent = dice;
 }
 document.getElementById('roll').addEventListener('click', Dodice );
 
