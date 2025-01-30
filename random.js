53f7ae2ab8fec7ea0049cf265a7f8f45d8ed48a0
yuta-33 <yuta33.0501@gmail.com>
2025-01-30 13:12:32 +0900

    Update random.js

--- random.js
+++ random.js
@@ -4,3 +4,14 @@ const Dodice = () =>{
 }
 document.getElementById('roll').addEventListener('click', Dodice );
 
+function fadeIn(element, duration = 1000) {
+            element.style.display = "block";
+            setTimeout(() => {
+                element.style.opacity = 1;
+            }, 10);
+        }
+
+        document.getElementById("roll").addEventListener("click", () => {
+            const image = document.getElementById("image");
+            fadeIn(image, 1000);
+        });
